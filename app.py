51e0c2e0247880afea02ec2d423f4ec255807ce9
minjae <minjagot@gmail.com>
2024-08-07 15:12:10 +0900

    Create jwt modul

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, make_response
 app = Flask(__name__)
 
 import os
@@ -16,7 +16,8 @@ jwt = JWTManager(app)
 
 import requests
 from bs4 import BeautifulSoup
-from source.auth.jwt_token import create_token
+from source.auth.jwt_token import create_token, unauthorized_response, convert_cookie_2_jwt
+from datetime import datetime, timedelta
 
 from pymongo import MongoClient
 client = MongoClient(mongodb_url)
@@ -30,22 +31,53 @@ def home():
 
 @app.route('/qna/<id>')
 def go_user_tetail(id):
+   print('qna in')
+   temp = request.cookies.get('jwt')
+   
+   print(temp)
+   jwt = convert_cookie_2_jwt(request.cookies)
+   print(jwt)
+   if jwt is None:
+       unauthorized_response()
    return render_template('detail.html')
+
+@app.errorhandler(401)
+def unathentication(error):
+    print(error)
+    return unauthorized_response()
+
+@app.errorhandler(422)
+def jwt_expires(error):
+    print(error)
+    return unauthorized_response()
 
 @app.route('/login', methods=['GET'])
 def login_success():
    #토큰에서 유저네임 꺼내기. 
-   user="김혜다"
-   return render_template('main.html', name=user)
+   jwt = convert_cookie_2_jwt(request.cookies)
+   
+   if jwt is None:
+      return unauthorized_response()
+   
+   name = jwt['name']
+   return render_template('main.html', name=name)
 
 @app.route('/login', methods=['POST'])
 def login_user():
    user_id = request.form['ID']
    user_pw = request.form['PW']
 
-   user = db.user.find_one({'user_id':user_id,'user_pw':user_pw})
+   user = db.user.find_one({'user_id':user_id,'user_pw':user_pw}, {'_id':False})
+
    if user:
-        return jsonify({'result': 'success'})
+      jwt_token = create_token(user)
+      response = make_response(jsonify({'result': 'success'}))
+      expire_time = datetime.now() + timedelta(hours=24)
+      max_age = 60 * 60 * 24
+
+      response.set_cookie('jwt', jwt_token, httponly=True, samesite='Strict', max_age=max_age, secure=True)
+      response.set_cookie('expires', str(expire_time), httponly=True, samesite='Strict', max_age=max_age, secure=True)
+      return response
    else:
         return jsonify({'result': 'fail'})
 
@@ -57,6 +89,12 @@ def recive_token_test():
    current_user_id = get_jwt_identity()
    return current_user_id
 
+@app.route('/main/nologin')
+def go_main_if_nologin():
+   response = make_response(render_template('main.html', msg='로그인이 필요합니다.'))
+   response.delete_cookie('jwt')
+   response.delete_cookie('expires')
+   return response
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
